fc5bb291ecfde86c0a4abcf695de0a3947d20851
Mohammad Rasheed <rasheed.mohd1526@gmail.com>
2025-08-23 11:32:30 +0530

    Added new components, utils, exception, logger, and pipeline files

---
+++ src/exception.py
@@ -0,0 +1,23 @@
+# Read exception handling in python documentation on https://docs.python.org/3/tutorial/errors.html
+import sys
+
+
+#creating a function to get the error message details
+def error_message_detail(error,error_detail:sys):
+    _,_,exc_tb=error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+    file_name,exc_tb.tb_lineno,str(error)
+    )
+    return error_message
+
+#creating a custom exception class to handle exceptions/errors
+class CustomException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message,error_detail) #calling the function to get the error message details
+
+    def __str__(self): #overriding the __str__ method to return the error message
+        return self.error_message
+
+#this code is for testing the custom exception class
